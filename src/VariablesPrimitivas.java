3be439e31e68a4258ebc257b261d0db133258162
JuanMontoya <@JuanDavid631>
2023-10-26 15:27:46 -0500

    VariablesPrimitivas_Enteras

---
+++ src/VariablesPrimitivas.java
@@ -0,0 +1,11 @@
+public class VariablesPrimitivas {
+  public static void main(String[] args) {
+    int a = 10;
+    long b = 20;
+    byte c = 25;
+    /* Variables enteras */
+    System.out.println("La suma de los tres números es: " + (a + b + c));
+    System.out.println(a / b);
+    System.out.println(c / b);
+  }
+}
