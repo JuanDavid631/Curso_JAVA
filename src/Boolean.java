cb329ef06b3f200569c594942905e524afc3d63a
JuanMontoya <@JuanDavid631>
2023-10-26 16:00:03 -0500

    Boolean

---
+++ src/Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean {
+  public static void main(String[] args) {
+    boolean condicion = true;
+    boolean condicionDos = false;
+    boolean verificiar = 3 == 2 + 1;
+    System.out.println(verificiar + " " + condicionDos);
+  }
+}
