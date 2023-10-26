6db94f13bcfb83daddb7de2e36be23faed7df7ba
JuanMontoya <@JuanDavid631>
2023-10-26 15:14:02 -0500

    Variables_Primitivas

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 20;
+        long a = 400000000;
+        double b = 4.56, c = 4.5;
+        String nombre = "Abraham";
+        /*int [] arreglo = new int[];*/
+        System.out.println(edad);
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
