c12e006eb4c4786ee9a9ed5b6d2eecc79c5b0196
JuanMontoya <@JuanDavid631>
2023-10-26 15:42:22 -0500

    VariablesPrimitivas_Char

---
+++ src/Variables_Char.java
@@ -0,0 +1,13 @@
+public class Variables_Char {
+  public static void main(String[] args) {
+    // UNICODE
+      char simboloTres = '\u0026';
+    // ASCII - Combinacion de teclas en el teclado
+      char simboloDos = 38;
+    // CAR - Simbologia como el teclado lo muestra
+      char simbolo = '&';
+    System.out.println("Codigo CAR: " + simbolo);
+    System.out.println("Codigo ASCII: " + simboloDos);
+    System.out.println("Codigo UNICODE: " + simboloTres);
+  }
+}
