e000f701be8818f9cdc5bb227a805f853056dc70
JuanMontoya <@JuanDavid631>
2023-10-30 17:53:46 -0500

    Variables: VariablesPrimitivas

--- src/VariablesPrimitivas_Flotantes.java
+++ src/VariablesPrimitivas_Flotantes.java
@@ -2,9 +2,9 @@ public class VariablesPrimitivas_Flotantes {
   public static void main(String[] args) {
     /* Float se usa para notacion cientifica y hacer conversiones con otras escalas de medidas */
     float variable = 2.5e2F;
-      System.out.println(variable);
+        System.out.println(variable);
     /* Manejar double cuando se requiere decimales puntales */
     double variableDos = 2.67;
-      System.out.println(variableDos);
+        System.out.println(variableDos);
   }
 }
