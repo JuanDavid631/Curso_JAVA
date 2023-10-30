32b4d6cf1c1013c50fa6ab82aa6ccb5363d9c21d
JuanMontoya <@JuanDavid631>
2023-10-30 18:14:52 -0500

    Strings: Metodos Importantes

--- src/MetodosImportantes_Strings.java
+++ src/MetodosImportantes_Strings.java
@@ -1,5 +1,14 @@
+import java.util.Locale;
+
 public class MetodosImportantes_Strings {
   public static void main(String[] args) {
-  
+    String nombre = "Juan";
+      System.out.println(nombre.toUpperCase(Locale.ROOT) + " --> Conversion a mayusculas");
+      System.out.println(nombre.toLowerCase(Locale.ROOT) + " --> Conversion a minusculas");
+      System.out.println(nombre.equals("juan") + " --> Valida el contenido de la variable");
+      System.out.println(nombre.equalsIgnoreCase("juan") + " --> Ignora como este declarada la variables");
+      System.out.println(nombre.compareTo("Andres") + " --> Comparacion de pasos para llegar de una palabra a otra");
+      System.out.println(nombre.charAt(0) + " --> Primer caracter de la variable");
+      System.out.println(nombre.charAt(nombre.length()-1));
   }
 }
