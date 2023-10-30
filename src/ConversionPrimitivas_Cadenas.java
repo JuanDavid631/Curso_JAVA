da8e5e44d220e156912788b8c26319e4605fa916
JuanMontoya <@JuanDavid631>
2023-10-30 16:53:21 -0500

    Conversion2

--- src/ConversionPrimitivas_Cadenas.java
+++ src/ConversionPrimitivas_Cadenas.java
@@ -2,15 +2,13 @@ public class ConversionPrimitivas_Cadenas {
   public static void main(String[] args) {
     boolean cierto = true;
     String ciertoConvertido = Boolean.toString(cierto);
-      System.out.println(ciertoConvertido);
     double decimals = 2.56;
     String decimalsConvertido = Double.toString(decimals);
-      System.out.println(decimalsConvertido);
     int entero = 395;
     String enteroConvertido = Integer.toString(entero);
-      System.out.println(enteroConvertido);
     float flotante = 2.56e2f;
     String flotanteConvertido = Float.toString(flotante);
-      System.out.println(flotanteConvertido);
+    String mensaje = ciertoConvertido + "\n" + decimalsConvertido + "\n" + enteroConvertido + "\n" + flotanteConvertido;
+      System.out.println(mensaje);
   }
 }
