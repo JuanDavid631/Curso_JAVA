4799a086d6a5559fb1af4e36e6f731fd2a0281b1
JuanMontoya <@JuanDavid631>
2023-10-30 17:54:26 -0500

    Variables: ConversionCadenas_Primitivas

--- src/ConversionCadenas_Primitivas.java
+++ src/ConversionCadenas_Primitivas.java
@@ -5,12 +5,12 @@ public class ConversionCadenas_Primitivas {
       // System.out.println(booleanConvertido);
     String decimals = "2.56";
     double decimalsConvertido = Double.parseDouble(decimals);
-      System.out.println(decimalsConvertido);
+        System.out.println(decimalsConvertido);
     String entero = "395";
     int enteroConvertido = Integer.parseInt(entero);
-      System.out.println(enteroConvertido);
+        System.out.println(enteroConvertido);
     String flotante = "2.56e2f";
     float flotanteConvertido = Float.parseFloat(flotante);
-      System.out.println(flotanteConvertido);
+        System.out.println(flotanteConvertido);
   }
 }
