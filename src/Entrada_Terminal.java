2fd51cd846edcd4a9135070188f6cd30ce175a76
JuanMontoya <@JuanDavid631>
2023-10-30 17:48:25 -0500

    Variables: EntradaDatos_Terminal

--- src/Entrada_Terminal.java
+++ src/Entrada_Terminal.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Entrada_Terminal {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
-      System.out.println("Introduce un numero entero: ");
+        System.out.println("Introduce un numero entero: ");
     int numeroReal = entrada.nextInt();
-      System.out.print("Nuestro numero es: " + numeroReal);
+        System.out.print("Nuestro numero es: " + numeroReal);
     String numeroBinario = Integer.toBinaryString(numeroReal);
-      System.out.println("Numero en binario: " + numeroBinario);
-      System.out.println("Numero en hexadecimal: " + Integer.toHexString(numeroReal));
-      System.out.println("Numero en Octal: " + Integer.toOctalString(numeroReal));
+        System.out.println("Numero en binario: " + numeroBinario);
+        System.out.println("Numero en hexadecimal: " + Integer.toHexString(numeroReal));
+        System.out.println("Numero en Octal: " + Integer.toOctalString(numeroReal));
   }
 }
