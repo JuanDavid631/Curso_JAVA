41631bd15ac28b89f8b01bcc5a6b5a575b050d92
JuanMontoya <@JuanDavid631>
2023-10-30 18:18:32 -0500

    Strings: Metodos Importantes

--- src/MetodosImportantes_Strings.java
+++ src/MetodosImportantes_Strings.java
@@ -9,6 +9,9 @@ public class MetodosImportantes_Strings {
       System.out.println(nombre.equalsIgnoreCase("juan") + " --> Ignora como este declarada la variables");
       System.out.println(nombre.compareTo("Andres") + " --> Comparacion de pasos para llegar de una palabra a otra");
       System.out.println(nombre.charAt(0) + " --> Primer caracter de la variable");
-      System.out.println(nombre.charAt(nombre.length()-1));
+      System.out.println(nombre.charAt(nombre.length()-1) + " --> Ultimo caracter de la variable");
+      System.out.println(nombre.compareToIgnoreCase("Andres")+ " --> Ignora la comparacion de pasos para llegar de una palabra a otra");
+      System.out.println(nombre.replace("J", "P") + " --> Reemplaza lo que se le pide");
+      
   }
 }
