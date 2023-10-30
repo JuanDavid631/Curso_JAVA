a74f0bd19e1f14077f1e9c49841d6bf24cb84295
JuanMontoya <@JuanDavid631>
2023-10-30 17:44:20 -0500

    Strings: Variables

--- src/Strings.java
+++ src/Strings.java
@@ -3,10 +3,10 @@ public class Strings {
     String hola = "Hola, gracias por la informacion";
     String holaDos = "Hola, gracias por la informacion";
     String holaUno = new String("Hola, gracias por la informacion");
-      System.out.println(hola + "--> Misma declaracion de variable" + "\n" + holaDos + "--> Misma declaracion de variable");
-      System.out.println(holaUno + "--> Declaracion de variable distinta");
-      System.out.print("Misma declaracion: ");
-      System.out.println(hola == holaDos);
-      System.out.println("Declaracion distinta: " + hola.equals(holaUno) + " y evalua por lo que esta adentro");
+        System.out.println(hola + "--> Misma declaracion de variable" + "\n" + holaDos + "--> Misma declaracion de variable");
+        System.out.println(holaUno + "--> Declaracion de variable distinta");
+        System.out.print("Misma declaracion: ");
+        System.out.println(hola == holaDos);
+        System.out.println("Declaracion distinta: " + hola.equals(holaUno) + " y evalua por lo que esta adentro");
   }
 }
