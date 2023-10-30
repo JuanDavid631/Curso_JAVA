e163511619c6042ac30d02d6bbeac6e21f05fb11
JuanMontoya <@JuanDavid631>
2023-10-30 17:45:55 -0500

    Variables: Conversion_DatosPrimitivos

--- src/Conversion_DatosPrimitivos.java
+++ src/Conversion_DatosPrimitivos.java
@@ -2,10 +2,10 @@ public class Conversion_DatosPrimitivos {
   public static void main(String[] args) {
     int numero = 400;
     short corto = (short)numero;
-      System.out.println(corto);
+        System.out.println(corto);
     long largo = numero;
-      System.out.println(largo);
+        System.out.println(largo);
     char enteros = (char)numero;
-      System.out.println(enteros);
+        System.out.println(enteros);
   }
 }
