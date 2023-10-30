eed7f4b9a34c2e6422b4e1c8689bb7fc618a28f7
JuanMontoya <@JuanDavid631>
2023-10-30 17:02:09 -0500

    Conversion3

---
+++ src/Conversion_DatosPrimitivos.java
@@ -0,0 +1,11 @@
+public class Conversion_DatosPrimitivos {
+  public static void main(String[] args) {
+    int numero = 400;
+    short corto = (short)numero;
+      System.out.println(corto);
+    long largo = numero;
+      System.out.println(largo);
+    char enteros = (char)numero;
+      System.out.println(enteros);
+  }
+}
