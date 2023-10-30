fb757a0f3c27449fa9b4a9e03adc90c3349f32b9
JuanMontoya <@JuanDavid631>
2023-10-30 18:30:56 -0500

    Strings: Metodos Importantes

--- src/MetodosImportantes_Strings.java
+++ src/MetodosImportantes_Strings.java
@@ -12,6 +12,15 @@ public class MetodosImportantes_Strings {
       System.out.println(nombre.charAt(nombre.length()-1) + " --> Ultimo caracter de la variable");
       System.out.println(nombre.compareToIgnoreCase("Andres")+ " --> Ignora la comparacion de pasos para llegar de una palabra a otra");
       System.out.println(nombre.replace("J", "P") + " --> Reemplaza lo que se le pide");
-      
+      System.out.println(nombre.lastIndexOf("u") + " --> Busqueda del caracter y me indica la posicion");
+      System.out.println(nombre.indexOf("an") + " --> Index donde encuentra dicho caracter");
+      System.out.println(nombre.startsWith("an") + " --> Valida con que inicia la variable");
+      System.out.println(nombre.startsWith("Ju") + " --> Valida con que inicia la variable");
+      System.out.println(nombre.endsWith("uan") + " --> Valida con que finaliza la variable");
+      System.out.println(nombre.endsWith("J") + " --> Valida con que finaliza la variable");
+      System.out.println(nombre.contains("ra") + " --> Valida que lo que se especifica esta dentro  variable");
+      String nombreDos = "     Laura";
+      System.out.println(nombreDos + " --> Variable sin ninguna modificacion");
+      System.out.println(nombreDos.trim() + " --> Quita los espacios que se encuentren antes");
   }
 }
