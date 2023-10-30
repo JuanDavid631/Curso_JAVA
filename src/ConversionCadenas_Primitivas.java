6a947fb1c50064c51a37950ca977aa6f14a69636
JuanMontoya <@JuanDavid631>
2023-10-30 17:36:45 -0500

    Manejo formateo

--- src/ConversionCadenas_Primitivas.java
+++ src/ConversionCadenas_Primitivas.java
@@ -1,8 +1,8 @@
 public class ConversionCadenas_Primitivas {
   public static void main(String[] args) {
     String booleano = "true";
-    Boolean booleanConvertido = Boolean.parseBoolean(booleano);
-      System.out.println(booleanConvertido);
+    // Boolean booleanConvertido = Boolean.parseBoolean(booleano);
+      // System.out.println(booleanConvertido);
     String decimals = "2.56";
     double decimalsConvertido = Double.parseDouble(decimals);
       System.out.println(decimalsConvertido);
--- src/ConversionPrimitivas_Cadenas.java
+++ src/ConversionPrimitivas_Cadenas.java
@@ -1,14 +1,14 @@
 public class ConversionPrimitivas_Cadenas {
   public static void main(String[] args) {
     boolean cierto = true;
-    String ciertoConvertido = Boolean.toString(cierto);
+    // String ciertoConvertido = Boolean.toString(cierto);
     double decimals = 2.56;
     String decimalsConvertido = Double.toString(decimals);
     int entero = 395;
     String enteroConvertido = Integer.toString(entero);
     float flotante = 2.56e2f;
     String flotanteConvertido = Float.toString(flotante);
-    String mensaje = ciertoConvertido + "\n" + decimalsConvertido + "\n" + enteroConvertido + "\n" + flotanteConvertido;
+    String mensaje = /*ciertoConvertido + "\n" + */decimalsConvertido + "\n" + enteroConvertido + "\n" + flotanteConvertido;
       System.out.println(mensaje);
   }
 }
