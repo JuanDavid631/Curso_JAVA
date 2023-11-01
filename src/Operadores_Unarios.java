406f30b6397d8bba77946c9d254608de943d964a
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:26:09 -0500

    Operadores: Unarios

---
+++ src/Operadores_Unarios.java
@@ -0,0 +1,7 @@
+public class Operadores_Unarios {
+  public static void main(String[] args) {
+    int numero =- 2;
+    int cambio = -numero;
+    System.out.println("Variable sin cambio de signo: " + numero + "\nVariable con cambio de signo: " + cambio);
+  }
+}
