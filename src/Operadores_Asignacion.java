9c9555a49443bb8c66fdf14ebfef778a635b00c0
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:24:09 -0500

    Operadores: Asignacion

--- src/Operadores_Asignacion.java
+++ src/Operadores_Asignacion.java
@@ -1,8 +1,11 @@
 public class Operadores_Asignacion {
   public static void main(String[] args) {
-    int uno = 5;
+    int uno = 5, dos = 12, tres = 8;
     int incremental = uno + 2;
-
-      System.out.println(uno);
+    uno += 2;
+    uno += 5;
+    dos -= 4;
+    tres *= 3;
+      System.out.println("Incremental: " + uno + "\nDisminucion: " + dos + "\nMultiplicacion: " + tres);
   }
 }
