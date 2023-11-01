be7610e28fe4f5b6c7388bd53246d2299c1a9c11
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:48:44 -0500

    Operadores: Relacionales

--- src/Operadores_Relacionales.java
+++ src/Operadores_Relacionales.java
@@ -7,7 +7,11 @@ public class Operadores_Relacionales {
     boolean mentira = false;
     boolean relacionalCinco = (true == mentira);
     boolean relacionalSeis = (true != mentira);
+    boolean validar = (valor < numero);
+    boolean validarDos = (valor > numero);
+
       System.out.println("Logica inicial: " + relacional + "\nCambio de logica: " + relacionalDos + "\nDistinto a algo: " + relacionalTres
-                          + "\nValidaciones: " + relacionalCinco+ "\nValidaciones: " + relacionalSeis);
+                          + "\nValidaciones: " + relacionalCinco + "\nValidaciones: " + relacionalSeis + "\nValidaciones Mayor y Menor que: " + validar
+                          + "\nValidaciones Mayor y Menor que: " + validarDos);
   }
 }
