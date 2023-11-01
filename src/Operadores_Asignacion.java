66cc93c77f5f66baf642f077a86c48873655cfd1
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:19:42 -0500

    Operadores: Asignacion

--- src/Operadores_Asignacion.java
+++ src/Operadores_Asignacion.java
@@ -1,12 +1,8 @@
 public class Operadores_Asignacion {
   public static void main(String[] args) {
-    int uno = 5, dos = 7;
-    int suma = uno + dos;
-    int resta = uno - dos;
-    int multiplicacion = uno * dos;
-    float division = (float) uno / (float) dos;
-    int resto = uno % dos;
-    System.out.println("La suma es: " + suma + "\nLa resta es: " + resta + "\nLa multiplicacion es: " + multiplicacion +
-        "\nLa division es: " + division + "\nEl resto es: " + resto);
+    int uno = 5;
+    int incremental = uno + 2;
+
+      System.out.println(uno);
   }
 }
