07c88a946348909604bc114521b431dd3429956e
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 18:07:21 -0500

    Operadores: Logicos

--- src/Operadores_Logicos.java
+++ src/Operadores_Logicos.java
@@ -10,5 +10,12 @@ public class Operadores_Logicos {
       System.out.println("Validacion primera que se cumpla una condicion: " + validar + "\nValidacion segunda que se cumpla una condicion: " + validarDos
                           + "\nValidacion tercera que se cumplan ambas condiciones: " + validarTres + "\nValidacion cuarta que se cumplan ambas condiciones: " + validarCuatro);
       System.out.println("Validaciones logicas o numericas: " + otro);
+
+    boolean nuevo = (valor > numero || valor == numero) || (mentira == false);
+    boolean nuevoDos = (valor < numero || valor == numero) || (mentira == true);
+    boolean nuevoTres = ((valor > numero && mentira == true) || (valor == numero && valor < numero)) && (mentira == false);
+    boolean nuevoCuatro = ((valor > numero && mentira == false) || (valor == numero && valor < numero)) && (mentira == false);
+      System.out.println("\nPrecedencia de los operadores: " + nuevo + "\nPrecedencia de los operadores: " + nuevoDos + "\nPrecedencia de los operadores: " + nuevoTres
+                        + "\nPrecedencia de los operadores: " + nuevoCuatro);
   }
 }
