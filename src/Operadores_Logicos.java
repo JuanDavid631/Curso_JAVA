bdc10275d00a55ef91e87e6369a7d647e27e2146
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:56:58 -0500

    Operadores: Logicos

--- src/Operadores_Logicos.java
+++ src/Operadores_Logicos.java
@@ -1,5 +1,10 @@
 public class Operadores_Logicos {
   public static void main(String[] args) {
+    int valor = 7, numero = 5;
+    boolean mentira = false;
+    boolean validar = (valor < numero || valor == numero);
+    boolean validarDos = (valor > numero || valor == numero);
 
+      System.out.println("Validacion primera: " + validar + "\nValidacion segunda: " + validarDos);
   }
 }
