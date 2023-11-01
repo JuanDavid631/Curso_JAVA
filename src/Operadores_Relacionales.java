eb71d768b838af688fe74efe2b03bb5b322841cb
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:43:43 -0500

    Operadores: Relacionales

---
+++ src/Operadores_Relacionales.java
@@ -0,0 +1,8 @@
+public class Operadores_Relacionales {
+  public static void main(String[] args) {
+    int valor = 7, numero= 5;
+    boolean relacional = (valor == numero);
+    boolean relacionalDos = !relacional;
+      System.out.println("Logica inicial: " + relacional + "\nCambio de logica: " + relacionalDos);
+  }
+}
