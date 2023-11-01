6b5d6a1bafce7470ba365b60f883897f21fbf6a2
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:34:31 -0500

    Operadores: Incremento_Decremento

--- src/Operadores_Incremento_Decremento.java
+++ src/Operadores_Incremento_Decremento.java
@@ -1,5 +1,8 @@
 public class Operadores_Incremento_Decremento {
   public static void main(String[] args) {
-
+    int valor = 5;
+    // Incremento en uno
+    int incremento = ++valor;
+      System.out.println("Numero incrementado: " + incremento);
   }
 }
