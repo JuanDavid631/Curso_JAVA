e185afbe5eca9b3d7d074ddf03406fcb075b2b4f
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 18:37:04 -0500

    Operadores: InstanceOf

--- src/Operadores_Instanceof.java
+++ src/Operadores_Instanceof.java
@@ -1,5 +1,14 @@
 public class Operadores_Instanceof {
   public static void main(String[] args) {
-
+    String texto = "Hola, ¿como estas?";
+    Integer numero = 6;
+    boolean validar = texto instanceof String;
+      System.out.println(validar);
+    validar = texto instanceof Object;
+      System.out.println(validar);
+    validar = numero instanceof Integer;
+      System.out.println(validar);
+    validar = numero instanceof Object;
+      System.out.println(validar);
   }
 }
