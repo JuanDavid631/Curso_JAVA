50f210febc82c2ae0c960283df02bd7de4214605
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 18:11:48 -0500

    Operadores: Logicos Ejercicio

--- src/Exercise_Login.java
+++ src/Exercise_Login.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Exercise_Login {
   public static void main(String[] args) {
+    String user = "Juan", password = "12345";
+    Scanner entrada = new Scanner(System.in);
+      System.out.println("Introduce tu usuario: ");
+    String usuario = entrada.nextLine();
+      System.out.println("Introduce tu contraseña: ");
+    String contrasena = entrada.nextLine();
 
   }
 }
