8c35f51c54502132845e2062d2138dbe4065cc52
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:38:30 -0500

    Operadores: Incremento_Decremento

--- src/Operadores_Incremento_Decremento.java
+++ src/Operadores_Incremento_Decremento.java
@@ -1,8 +1,21 @@
 public class Operadores_Incremento_Decremento {
   public static void main(String[] args) {
     int valor = 5;
-    // Incremento en uno
+    // Incremento en uno y luego devuelve el numero
     int incremento = ++valor;
       System.out.println("Numero incrementado: " + incremento);
+    // Devuelve el numero y luego incrementa
+    valor = 5;
+    int numeroAntes = valor++;
+      System.out.println("Numero incrementado a validar : " + numeroAntes);
+
+    // Decrementa en uno y luego devuelve el numero
+    valor = 5;
+    int decremento = --valor;
+    System.out.println("Numero decrementado: " + decremento);
+    // Devuelve el numero y luego incrementa
+    valor = 5;
+    int numeroDecrementaAntes = valor--;
+    System.out.println("Numero decrementado a validar : " + numeroDecrementaAntes);
   }
 }
