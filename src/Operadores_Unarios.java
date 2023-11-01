df0377b05f7f683e70b079a9709921c149dce758
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 17:28:13 -0500

    Operadores: Unarios

--- src/Operadores_Unarios.java
+++ src/Operadores_Unarios.java
@@ -1,7 +1,8 @@
 public class Operadores_Unarios {
   public static void main(String[] args) {
-    int numero =- 2;
-    int cambio = -numero;
-    System.out.println("Variable sin cambio de signo: " + numero + "\nVariable con cambio de signo: " + cambio);
+    int numero =- 2, valor = -4;
+    int cambio = -numero, modificar = +valor;
+    System.out.println("Variable sin cambio de signo: " + numero + "\nVariable con cambio de signo: " + cambio +
+                        "\nVariable sin cambio de signo: " + valor + "\nVariable con cambio de signo: " + modificar);
   }
 }
