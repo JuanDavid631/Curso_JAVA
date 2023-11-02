5efda8a6dab1d304cdaf406d9819c795c8d0eea5
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 21:18:26 -0500

    Operadores: InstanceOf

--- src/Operadores_Instanceof.java
+++ src/Operadores_Instanceof.java
@@ -10,5 +10,17 @@ public class Operadores_Instanceof {
       System.out.println(validar);
     validar = numero instanceof Object;
       System.out.println(validar);
+
+    Number numeroDos = 9;
+    validar = numeroDos instanceof Long;
+      System.out.println(validar);
+    validar = numeroDos instanceof  Double;
+      System.out.println(validar);
+    validar = numeroDos instanceof Float;
+      System.out.println(validar);
+
+    Number numeroTres = 6.32f;
+    validar = numeroTres instanceof  Double;
+      System.out.println(validar);
   }
 }
